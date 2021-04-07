07744305f2677fa3c0c517052a95e6f3e16122f8
jk0527 <78495599+jk0527@users.noreply.github.com>
2021-04-07 10:38:05 +0900

    Add files via upload

---
+++ Seminar2104/cifar_cnn_keras_plot.py
@@ -0,0 +1,94 @@
+from tensorflow import keras
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+from keras.utils import np_utils
+from keras.layers.normalization import BatchNormalization
+ 
+# csv을 위한 라이브러리
+import time
+import csv
+
+batch_size = 100
+num_classes = 10
+epochs = 30
+trainingDataSize = 50000
+batch_size = 100
+display_step = 1
+ 
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# One hot Encoding
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+ 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+ 
+model.add(Flatten())
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+ 
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+ 
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+ 
+class TimeHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, epoch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
+
+
+write_enable = 1
+if write_enable == 1:
+    sheet_file = open('C:/Users/inslab/Desktop/Soomin/cifar_cnn_relu_adam.csv', 'w+', newline='')
+    wr = csv.writer(sheet_file)
+time_accumulate = []
+for i in range(epochs):
+    time_accumulate.append(0)
+time_callback = TimeHistory()
+hist = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size, callbacks=[time_callback])
+
+for i in range(epochs):
+    for j in range(0, i+1):
+        time_accumulate[i] += time_callback.times[j]
+print(time_callback.times)
+print(time_accumulate)
+with open('./result_test', 'wb') as f:
+    for i in range(epochs):
+        wr.writerow([i, time_accumulate[i], 100*hist.history['val_acc'][i], hist.history['loss'][i]])    
+print('====================')
+
+
+if write_enable == 1:
+            sheet_file.close()
